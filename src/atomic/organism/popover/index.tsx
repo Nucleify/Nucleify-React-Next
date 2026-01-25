b696c91eb18ba28f4b4750470876e3ddd0b05051
Mateusz Szymanski <mszymanski027@gmail.com>
2026-01-25 12:29:28 +0100

    feat(atomic_design): create atomic component for Popover

---
+++ src/atomic/organism/popover/index.tsx
@@ -0,0 +1,70 @@
+import type { JSX, ReactNode } from 'react'
+import { useRef } from 'react'
+
+import { OverlayPanel } from 'primereact/overlaypanel'
+import { AdButton } from '@/atomic/atom/button'
+import type { PopoverInterface } from './types'
+
+const EXCLUDED_PROPS = [
+  'src',
+  'buttonClass',
+  'buttonStyle',
+  'popoverClass',
+  'icon',
+]
+
+function filterProps<T extends object>(
+  props: T,
+  exclude: string[]
+): Partial<T> {
+  const filtered = {} as Partial<T>
+  ;(Object.keys(props) as Array<keyof T>).forEach((key) => {
+    if (!exclude.includes(key as string)) filtered[key] = props[key]
+  })
+  return filtered
+}
+
+export default function AdPopover({
+  className,
+  children,
+  position,
+  src,
+  buttonClass,
+  buttonStyle,
+  buttonText,
+  popoverClass,
+  icon,
+  ...props
+}: PopoverInterface & { children?: ReactNode }): JSX.Element {
+  const opRef = useRef<OverlayPanel>(null)
+  const showButton = buttonText || icon || src
+  const overlayPanelProps = {
+    ...filterProps(props, EXCLUDED_PROPS),
+    ref: opRef,
+    className: [className, popoverClass, 'ad-popover', position]
+      .filter(Boolean)
+      .join(' '),
+    'data-position': position,
+  }
+  return (
+    <>
+      {showButton && (
+        <AdButton
+          label={buttonText}
+          icon={icon}
+          src={src}
+          className={[buttonClass, 'ad-popover-toggle', position]
+            .filter(Boolean)
+            .join(' ')}
+          style={typeof buttonStyle === 'string' ? undefined : buttonStyle}
+          rounded
+          onClick={(e) => opRef.current?.toggle(e)}
+        />
+      )}
+      <OverlayPanel {...overlayPanelProps}>{children}</OverlayPanel>
+    </>
+  )
+}
+
+export type * from './types'
+export { AdPopover }
