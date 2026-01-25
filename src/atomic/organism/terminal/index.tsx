e2a983d9ed65f62c6bcdf6834d7a18994e0bf69b
Mateusz Szymanski <mszymanski027@gmail.com>
2026-01-25 12:52:03 +0100

    feat(atomic_design): create atomic component for Terminal

---
+++ src/atomic/organism/terminal/index.tsx
@@ -0,0 +1,34 @@
+import { JSX, useEffect } from 'react'
+
+import { Terminal } from 'primereact/terminal'
+import { TerminalService } from 'primereact/terminalservice'
+import type { TerminalInterface } from './types'
+import { handleCommands } from './utils/handle_commands'
+
+export default function AdTerminal({
+  className,
+  ...props
+}: TerminalInterface): JSX.Element {
+  useEffect(() => {
+    TerminalService.on('command', handleCommands)
+    return () => {
+      TerminalService.off('command', handleCommands)
+    }
+  }, [])
+
+  const pt = {
+    command: { className: 'ad-terminal-command' },
+    response: { className: 'ad-terminal-response' },
+  }
+
+  return (
+    <Terminal
+      {...props}
+      className={[className, 'ad-terminal'].filter(Boolean).join(' ')}
+      pt={pt}
+    />
+  )
+}
+
+export type * from './types'
+export { AdTerminal }
