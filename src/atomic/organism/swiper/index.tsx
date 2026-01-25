c38ff4aa9131d25f3fc2cb334abaf722aebba41e
Mateusz Szymanski <mszymanski027@gmail.com>
2026-01-25 13:18:41 +0100

    feat(atomic_design): create atomic component for Swiper

---
+++ src/atomic/organism/swiper/index.tsx
@@ -0,0 +1,31 @@
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
+import type { ReactNode } from 'react'
+
+import type { SwiperInterface } from './types'
+
+export default function AdSwiper(
+  props: SwiperInterface & { children?: ReactNode }
+) {
+  const { slides, children, ...rest } = props
+  return (
+    <div className="swiper-container ad-swiper">
+      <Swiper {...rest}>
+        {slides &&
+          slides.map((slide, idx) => (
+            <SwiperSlide key={idx}>
+              {slide.url && slide.image ? (
+                <a href={slide.url}>
+                  <img src={(slide.prefix || '') + slide.image} alt="" />
+                </a>
+              ) : null}
+            </SwiperSlide>
+          ))}
+        {children}
+      </Swiper>
+    </div>
+  )
+}
+
+export type * from './types'
+export { AdSwiper }
