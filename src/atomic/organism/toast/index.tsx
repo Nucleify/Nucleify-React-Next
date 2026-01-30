754530a1f6166c82fb7dca9e86351604fb0e64b7
Mateusz Szymanski <mszymanski027@gmail.com>
2026-01-30 23:31:22 +0100

    feat(atomic_design): add styles for AdToast

--- src/atomic/organism/toast/index.tsx
+++ src/atomic/organism/toast/index.tsx
@@ -1,6 +1,8 @@
+'use client'
 import type { ReactNode } from 'react'
 
 import { Toast } from 'primereact/toast'
+import styles from './index.module.scss'
 import type { ToastInterface } from './types'
 
 interface AdToastProps extends ToastInterface {
@@ -9,11 +11,21 @@ interface AdToastProps extends ToastInterface {
 }
 
 export function AdToast({ className, children, pt, ...rest }: AdToastProps) {
+  const cx = (...classes: (string | undefined | null | false)[]) =>
+    classes.filter(Boolean).join(' ')
+
+  const defaultPt = {
+    message: { className: styles['ad-toast-message'] },
+    messageContent: { className: styles['ad-toast-message-content'] },
+    summary: { className: styles['ad-toast-summary'] },
+    closeButton: { className: styles['ad-toast-close-button'] },
+  }
+
   return (
     <Toast
       {...rest}
-      pt={pt}
-      className={[className, 'ad-toast'].filter(Boolean).join(' ')}
+      className={cx(className, styles['ad-toast'])}
+      pt={{ ...defaultPt, ...pt }}
     >
       {children}
     </Toast>
