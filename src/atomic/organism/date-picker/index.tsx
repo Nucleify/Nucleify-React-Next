44bc147a6ec8c883e545280e23d0dee96ab174fa
Mateusz Szymanski <mszymanski027@gmail.com>
2026-01-31 01:26:14 +0100

    feat(atomic_design): add styles for AdDatePicker

--- src/atomic/organism/date-picker/index.tsx
+++ src/atomic/organism/date-picker/index.tsx
@@ -1,6 +1,8 @@
+'use client'
 import type { JSX } from 'react'
 
 import { Calendar } from 'primereact/calendar'
+import styles from './index.module.scss'
 import type { DatePickerInterface } from './types'
 
 export function AdDatePicker({
@@ -9,20 +11,25 @@ export function AdDatePicker({
   showOnFocus = true,
   ...rest
 }: DatePickerInterface): JSX.Element {
-  const mergedClassName = ['ad-datepicker', className].filter(Boolean).join(' ')
+  const cx = (...classes: (string | undefined | null | false)[]) =>
+    classes.filter(Boolean).join(' ')
+
+  const pt = {
+    input: {
+      root: {
+        className: styles['ad-inputtext'],
+        ...(adType ? { 'ad-type': adType } : {}),
+      },
+    },
+  }
 
   return (
     <Calendar
       {...rest}
       showOnFocus={showOnFocus}
-      className={mergedClassName || undefined}
+      className={cx(styles['ad-datepicker'], className)}
+      pt={pt}
       {...(adType ? { 'ad-type': adType } : {})}
-      pt={{
-        root: {
-          className: 'ad-inputtext',
-          ...(adType ? { 'ad-type': adType } : {}),
-        },
-      }}
     />
   )
 }
