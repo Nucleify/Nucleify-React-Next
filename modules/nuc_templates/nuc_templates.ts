6980896d6f4ac940c6fd5eee95a1800e1d099a49
Mateusz Szymanski <mszymanski027@gmail.com>
2026-02-11 10:13:27 +0100

    feat(nuc_templates): add home-link component

--- modules/nuc_templates/nuc_templates.ts
+++ modules/nuc_templates/nuc_templates.ts
@@ -1,3 +1,4 @@
 export { NucAuthors } from './components/authors'
 export { NucCube } from './components/cube'
 export { NucHexagonsButton } from './components/hexagons-button'
+export { NucHomeLink } from './components/home-link'
