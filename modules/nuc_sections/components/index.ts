937ed0110a73f4e169532b1232de87f526e4ae7a
Mateusz Szymanski <mszymanski027@gmail.com>
2026-02-12 22:04:37 +0100

    feat(nuc_sections): add footer component

--- modules/nuc_sections/components/index.ts
+++ modules/nuc_sections/components/index.ts
@@ -1,3 +1,4 @@
 export * from './contact'
 export * from './email-us'
 export * from './faq'
+export * from './footer'
--- modules/nuc_sections/nuc_sections.ts
+++ modules/nuc_sections/nuc_sections.ts
@@ -1,3 +1,4 @@
 export { NucSectionContact } from './components/contact'
 export { NucSectionEmailUs } from './components/email-us'
 export { NucSectionFaq } from './components/faq'
+export { NucSectionFooter } from './components/footer'
