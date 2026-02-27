1f4ccd09fd506d27e368b75e559dc92dbb5a221d
Mateusz Szymanski <mszymanski027@gmail.com>
2026-02-27 01:10:47 +0100

    feat(nuc_colors): initialize nuc_colors module with exports and README

---
+++ modules/nuc_colors/index.ts
@@ -0,0 +1,10 @@
+/**
+ *  Module's main file export
+ */
+
+/**
+ *  Folders exports
+ */
+export * from './atomic'
+export * from './nuc_colors'
+export * from './plugins'
---
+++ modules/nuc_colors/nuc_colors.ts
@@ -0,0 +1,3 @@
+export function registerNucColors(): void {
+  return undefined
+}
