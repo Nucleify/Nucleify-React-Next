a79d6989ef0175868f146ea8cc8d1e7374442675
Mateusz Szymanski <mszymanski027@gmail.com>
2026-03-11 07:57:52 +0100

    feat(nuc_loading): reproduce module in next

---
+++ modules/nuc_loading/types/Loading/variables.ts
@@ -0,0 +1,2 @@
+export type LoadingType = boolean | undefined
+export type LoadingRefType = boolean
---
+++ modules/nuc_loading/utils/use_loading.ts
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+
+import type { LoadingRefType, UseLoadingInterface } from 'nucleify'
+
+export function useLoading(): UseLoadingInterface {
+  const [loading, setLoadingState] = useState<LoadingRefType>(false)
+
+  function setLoading(state: boolean, timeout?: number): void {
+    if (timeout) {
+      setTimeout(() => {
+        setLoadingState(state)
+      }, timeout)
+    } else {
+      setLoadingState(state)
+    }
+  }
+
+  return {
+    loading,
+    setLoading,
+  }
+}
