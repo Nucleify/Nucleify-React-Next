f38792ed59be987824f58646ecdb428a1c8ad297
i11umii <kirpa.michael@gmail.com>
2026-05-01 18:12:18 +0200

    refactor: convert nuc_dock from submodule to regular directory

---
+++ nuc_dock/index.ts
@@ -0,0 +1,13 @@
+/**
+ *  Module's main file export
+ */
+
+/**
+ *  Folders exports
+ */
+export * from './constants'
+/**
+ *  Files exports
+ */
+export { default as NucDock } from './index.tsx'
+export * from './nuc_dock'
---
+++ nuc_dock/nuc_dock.ts
@@ -0,0 +1,5 @@
+import { NucDock } from '.'
+
+export function registerNucDock(): void {
+  void NucDock
+}
