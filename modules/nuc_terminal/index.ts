00db4b3e0159379d5284cfcc24774ce3658b4b4a
i11umii <kirpa.michael@gmail.com>
2026-05-01 17:36:17 +0200

    refactor(nuc_terminal): migrate from Nuxt to Next.js

---
+++ modules/nuc_terminal/index.ts
@@ -0,0 +1,15 @@
+/**
+ *  Module's main file export
+ */
+export * from './index.tsx'
+
+/**
+ *  Folders exports
+ */
+export * from './types'
+export * from './utils'
+
+/**
+ *  Files exports
+ */
+export { default as NucTerminal } from './index.tsx'
